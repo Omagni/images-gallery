87fdfcb3779bbc678d5ad836cbfbbe679793b729
Brandon Tsuchiya <balextsu@gmail.com>
2025-08-27 14:59:41 -0500

    display and delete images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,51 @@
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header'
-import Search from './components/Search'
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import ImageCard from "./components/ImageCard";
+import { Container, Row, Col } from "react-bootstrap";
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
-  const [word, setWord] = useState('');
+  const [word, setWord] = useState("");
+  const [images, setImages] = useState([]);
+
+  console.log(images);
 
   const handleSearchSubmit = (e) => {
-  e.preventDefault();
-  console.log(word);
-  fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((err) => {
-      console.log(err);
-    })
+    e.preventDefault();
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages([{ ...data, title: word }, ...images]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
-    setWord('');
-}
+    setWord("");
+  };
+
+  const handleDeleteimage = (id) => {
+    setImages(images.filter((image) => image.id !== id));
+  };
 
   return (
     <div>
-      <Header title="Images Gallery"/>
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
-
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3}>
+          {images.map((image, i) => (
+            <Col key={i} className="pb-3">
+              <ImageCard image={image} deleteImage={handleDeleteimage}/>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 }
